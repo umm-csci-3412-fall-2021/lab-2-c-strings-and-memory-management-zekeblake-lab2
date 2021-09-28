45429542c1f57a687de992d6ca55729f88a0b7b9
Zeke Krug <krug0102@morris.umn.edu>
2021-09-28 13:54:13 -0500

    Updated disemvowel.c and main.c In disemvowel.c, moved the helper function checkVowels to above the disemvowel function. In main.c, moved the line that assigns the result to be disemvowel(line) into the while loop.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -2,27 +2,6 @@
 #include <string.h>
 #include "disemvowel.h"
 
-char *disemvowel(char *str) {
-  int checkVowel(char);
-  char *result; // the array of char that has the vowels removed from str
-  int len, i, j; // len: the length of the given array
-  len = strlen(str);
-  // add one to len so there's room for the null terminator '\0'
-  result = (char*) calloc(len+1, sizeof(char));
-  // for loop copies all elements of str into result
-  // and adds the null terminator at result[len]
-  j=0;
-  for (i=0; i<len; i++){
-    if(checkVowel(str[i])==0)
-    {result[j] = str[i];
-     j++;}
-  }
-
-  result[len]='\0';
-
-  return result;
-}
-
 // Helper function checkVowel
 int checkVowel(char ch)
 {
@@ -32,4 +11,27 @@ int checkVowel(char ch)
     else
         return 0;
 
+}
+
+char *disemvowel(char *str) {
+  char *result; // the array of char that has the vowels removed from str
+  int len, i, j; // len: the length of the given array
+  len = strlen(str);
+  j = 0;
+  // add one to len so there's room for the null terminator '\0'
+  result = (char*) calloc(len+1, sizeof(char));
+  // for loop copies all elements of str into result
+  // and adds the null terminator at result[len]
+  for (i=0; i<len; i++)
+  {
+    if(checkVowel(str[i])==0)
+    {
+     result[j] = str[i];
+     j++;
+    }
+  }
+
+  result[len] = '\0';
+
+  return result;
 }
--- disemvowel/main.c
+++ disemvowel/main.c
@@ -10,8 +10,8 @@ int main(int argc, char *argv[]) {
 
   size = 100;
   line = (char*) malloc (size + 1);
-  result = disemvowel(line);
   while (getline(&line, &size, stdin) > 0) {
+    result = disemvowel(line);
     printf("%s\n", result);
     free(result);
   }
